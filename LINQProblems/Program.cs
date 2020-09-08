5696e46f770de7dd8d62b81014712b4d44d96477
NCRadtke00 <raddnc@gmail.com>
2020-09-08 13:11:58 -0500

    LINQ problems, 4 problems put in.

--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -10,6 +10,17 @@ namespace LINQProblems
     {
         static void Main(string[] args)
         {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth",/* “mathematics” */};
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+
+            List<string> classGrades = new List<string>()
+            {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+            };
+            //4.Write a function that takes in a string of letters(i.e. “Terrill”) and returns an alphabetically ordered string corresponding to the letter frequency(i.e. "E1I1L2R2T1")
         }
     }
 }
